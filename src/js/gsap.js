1360106854b0dc6088bfa5385214b5aa2d6a855a
Petra Markovic <petra.markovic@vegait.rs>
2023-02-27 11:25:52 +0100

    horizontal scroll section

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -2,26 +2,58 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 
 const gsapAnimations = {
-	images: document.querySelectorAll('.js-image'),
+	container: document.querySelector('.js-offers-container'),
+	slides: gsap.utils.toArray('.js-offers-slide'),
+	text: gsap.utils.toArray('.anim'),
+	mask: document.querySelector('.mask'),
 
 	init: function() {
-		window.addEventListener('load', () => {
-			if (window.innerWidth < 768) {
-				return;
-			}
-		})
+		// window.addEventListener('load', () => {
+		// 	if (window.innerWidth < 768) {
+		// 		return;
+		// 	} else {
+		// 	}
+		// })
+		this.horizontalScroll();
 	},
 
-	triggerAnimations: function() {
+	horizontalScroll: function() {
 		gsap.registerPlugin(ScrollTrigger);
-		gsap.to(this.images, {
-			scale: 1.5,
+		let scrollTween = gsap.to(this.slides, {
+			xPercent: -100 * (this.slides.length - 1),
+			ease: 'none',
 			scrollTrigger: {
-				trigger: '.banner',
-				start: 'top center',
-				scrub: true,
-				ease: 'slow'
+				trigger: this.container,
+				pin: true,
+				scrub: 1,
+				eng: '+=3000'
 			}
+		})
+
+		gsap.to(this.mask, {
+			width: '100%',
+			scrollTrigger: {
+				trigger: '.offers',
+				star: 'top left',
+				scrub: 1
+			}
+		})
+
+		this.slides.forEach(slide => {
+			const text = slide.querySelectorAll('.anim');
+			if(text.length === 0) {return;}
+			gsap.from(text, {
+				y: -130,
+				opacity: 0,
+				duration: 1,
+				ease: 'elastic',
+				stagger: 0.1,
+				scrollTrigger: {
+					trigger: slide,
+					containerAnimation: scrollTween,
+					start: 'left center'
+				}
+			})
 		})
 	}
 }
--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,13 @@ import '../assets/svg/logo.svg';
 import '../assets/svg/instagram.svg';
 import '../assets/svg/youtube.svg';
 import '../assets/svg/tiktok.svg';
+import '../assets/svg/arrow-right.svg';
+import '../assets/svg/arrow-right-primary.svg';
 import '../assets/images/banner.png';
 import '../assets/images/banner-img.png';
+import '../assets/images/offers-1.png';
+import '../assets/images/offers-2.png';
+import '../assets/images/offers-3.png';
 
 import header from './header';
 header.init();
